815c756b411b3935413cf013a7d2b03dc2bec6f4
Walker Zhao <zhaocework@gmail.com>
2019-05-22 09:37:01 +0800

    删除无用import

--- app/src/main/java/net/ischool/isus/broadcast/CMDBReceiver.kt
+++ app/src/main/java/net/ischool/isus/broadcast/CMDBReceiver.kt
@@ -3,7 +3,6 @@ package net.ischool.isus.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import net.ischool.isus.service.CMDBService
 
 /**
