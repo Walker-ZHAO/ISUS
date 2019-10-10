1b2bb063cb9ce42aeffde8d2db8e6d53b8d08af6
Walker Zhao <zhaocework@gmail.com>
2019-10-10 20:03:59 +0800

    增加读卡器类型

--- app/src/main/java/net/ischool/isus/CReaderType.kt
+++ app/src/main/java/net/ischool/isus/CReaderType.kt
@@ -15,6 +15,7 @@ class CReaderType {
         @JvmField val T_U2 = 2              /** USB，身份证IC二合一，华大HD-100 **/
         @JvmField val T_U3 = 3              /** USB，身份证，因纳伟盛刷卡器 **/
         @JvmField val T_U4 = 8              /** USB，纵横六合，M1卡 **/
+        @JvmField val T_U5 = 9              /** 海康威视读卡器 **/
         @JvmField val T_S1 = 4              /** 串口，纯IC，ttyS1，115220波特率，8字节长度:0x02 四字节卡号（ASCII） 0x0D 0x0A 0x03(绵竹) **/
         @JvmField val T_S2 = 5              /** 串口，纯IC，ttyS0，19200波特率，12字节长度，7-10字节为卡号（木兰） **/
         @JvmField val T_S3 = 6              /** 串口，纯IC，纵横六合 **/
