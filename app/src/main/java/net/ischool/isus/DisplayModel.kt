9145728661efd7f08d4035e5de9fca46b51c49b5
Walker Zhao <zhaocework@gmail.com>
2019-11-06 08:59:29 +0800

    增加控制台纯闸机通道Pad端显示类别

--- app/src/main/java/net/ischool/isus/DisplayModel.kt
+++ app/src/main/java/net/ischool/isus/DisplayModel.kt
@@ -12,5 +12,6 @@ class DisplayModel {
         @JvmField val LEGACY = "1"  /** 单屏显示，初版大屏UI **/
         @JvmField val PAD = "2"     /** 单屏显示，Pad适配UI **/
         @JvmField val ASSIST = "3"  /** 双屏显示，主屏：Pad适配UI；副屏：Pad适配对应副屏UI **/
+        @JvmField val PAD_SINGLE_GATE = "4" /** 单屏显示，Pad适配UI（纯闸机通道） **/
     }
 }
