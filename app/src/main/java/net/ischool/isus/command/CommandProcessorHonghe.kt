0edd14573daa2b28e4697453d4a41a6e6f1713c6
Walker Zhao <zhaocework@gmail.com>
2023-07-13 10:55:05 +0800

    集成鸿合SDK，调整鸿合命令解析器

--- app/src/main/java/net/ischool/isus/command/CommandProcessorHonghe.kt
+++ app/src/main/java/net/ischool/isus/command/CommandProcessorHonghe.kt
@@ -1,10 +1,11 @@
 package net.ischool.isus.command
 
 import android.content.Context
-import android.content.Intent
 import android.os.Environment
 import com.orhanobut.logger.Logger
-import com.seewo.sdk.SDKSystemHelper
+import com.xbh.sdk3.AppComm.AppCommHelper
+import com.xbh.sdk3.System.SystemHelper
+import com.xbh.sdk3.client.UserAPI
 import net.ischool.isus.SYSLOG_CATEGORY_RABBITMQ
 import net.ischool.isus.log.Syslog
 import net.ischool.isus.network.APIService
@@ -20,12 +21,56 @@ import java.io.IOException
  * Date: 2022/9/29
  */
 class CommandProcessorHonghe(context: Context): CommandProcessorCommon(context) {
+
+    private val systemHelper = SystemHelper()
+    private val appHelper = AppCommHelper()
+
+    init {
+        UserAPI.getInstance().init(context.applicationContext)
+    }
+
+    /**
+     * 重置
+     */
+    override fun reset(remoteUUID: String) {
+        val result = CommandResult(ICommand.COMMAND_RESET)
+        appHelper.apply {
+            clearApplicationCacheData(context.packageName)
+            clearApplicationUserData(context.packageName)
+        }
+        finish(result, remoteUUID)
+    }
+
     /**
      * 设备重启
      */
     override fun reboot(remoteUUID: String) {
         finish(CommandResult(ICommand.COMMAND_REBOOT), remoteUUID)
-        context.sendBroadcast(Intent("android.intent.action.reboot"))
+        systemHelper.reboot()
+    }
+
+    /**
+     * 命令方式进入指定页面
+     *
+     * Note：需要系统签名
+     */
+    override fun launchPage(component: String?, remoteUUID: String) {
+        val result = CommandResult(ICommand.COMMAND_LAUNCH_PAGE)
+        if (component == null) {
+            result.fail("Component is invalid")
+            finish(result, remoteUUID)
+            return
+        }
+        systemHelper.executeCommand("am start -n $component")
+        finish(CommandResult(ICommand.COMMAND_LAUNCH_PAGE), remoteUUID)
+    }
+
+    /**
+     * 从当前页返回
+     */
+    override fun backPage(remoteUUID: String) {
+        systemHelper.executeCommand("input keyevent BACK")
+        finish(CommandResult(ICommand.COMMAND_BACK), remoteUUID)
     }
 
     /**
@@ -44,9 +89,7 @@ class CommandProcessorHonghe(context: Context): CommandProcessorCommon(context) 
             callback = object :
                 StringCallback {
                 override fun onResponse(string: String) {
-                    context.sendBroadcast(Intent("com.android.lango.installapp").apply {
-                        putExtra("apppath", string)
-                    })
+                    appHelper.silentInstallApp(string)
                     finish(result, remoteUUID)
                 }
 
