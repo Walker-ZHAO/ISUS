a58c632845fc5cdbbf7c5db0e20889eb97bcd185
Walker Zhao <zhaocework@gmail.com>
2023-11-21 14:29:12 +0800

    兼容海康新设备的触摸节点

--- app/src/main/java/net/ischool/isus/Extension.kt
+++ app/src/main/java/net/ischool/isus/Extension.kt
@@ -67,6 +67,7 @@ fun Context.sleep() {
             InfoDisplayApi.disableBacklight()
             // 需要禁用触屏，否则触摸事件会下发至应用
             InfoSystemApi.execCommand("su & rm -rf /dev/input/event2")
+            InfoSystemApi.execCommand("su & rm -rf /dev/input/event4")
             // 使CPU进入节能模式
             InfoSystemApi.execCommand("su & echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor")
         }
