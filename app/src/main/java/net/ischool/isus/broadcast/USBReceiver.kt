591734210c1a12fbe2d1a536de3ec2a0bc9583cc
Walker Zhao <zhaocework@gmail.com>
2024-02-28 11:03:28 +0800

    实现U盘插入自动执行批处理命令功能

--- app/src/main/java/net/ischool/isus/broadcast/USBReceiver.kt
+++ app/src/main/java/net/ischool/isus/broadcast/USBReceiver.kt
@@ -3,8 +3,11 @@ package net.ischool.isus.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import com.google.gson.Gson
+import net.ischool.isus.model.Command
 import java.io.File
+import com.walker.anke.gson.fromJson
+import net.ischool.isus.command.CommandParser
 
 /**
  * U盘插入监听
@@ -14,18 +17,28 @@ import java.io.File
  * Date: 2024/2/27
  */
 class USBReceiver: BroadcastReceiver() {
+    companion object {
+        private const val COMMAND_FILE = "commander.json"
+    }
     private var usbMounted = false
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == Intent.ACTION_MEDIA_MOUNTED && !usbMounted) {
             usbMounted = true
-            // 获取U盘挂载点路径
-            val mountPath = intent.data?.path ?: ""
-            val usbDrive = File(mountPath)
-            if (usbDrive.exists() && usbDrive.isDirectory) {
-                val files = usbDrive.listFiles()
-                files?.forEach {
-                    Log.i("Walker", "file name: ${it.name}")
+            try {
+                // 获取U盘挂载点路径
+                val mountPath = intent.data?.path ?: ""
+                val usbDrive = File(mountPath)
+                if (usbDrive.exists() && usbDrive.isDirectory) {
+                    val commandFile = usbDrive.listFiles()?.firstOrNull { it.name == COMMAND_FILE }
+                    val fileContent = commandFile?.readText(Charsets.UTF_8) ?: ""
+                    // 解析文件内部命令列表
+                    val commandList = Gson().fromJson<Array<Command>>(fileContent)
+                    commandList.forEach {
+                        CommandParser.instance.processCommand(it)
+                    }
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
         if (intent?.action == Intent.ACTION_MEDIA_UNMOUNTED || intent?.action == Intent.ACTION_MEDIA_EJECT) {
