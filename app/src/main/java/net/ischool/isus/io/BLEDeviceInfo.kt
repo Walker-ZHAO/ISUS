ae80264b2723b843f8432defdddefd245713909e
Walker Zhao <zhaocework@gmail.com>
2025-05-20 13:27:24 +0800

    调整 IBeacon 读取服务，提供的数据结构字段名称，减少数据体积

--- app/src/main/java/net/ischool/isus/io/BLEDeviceInfo.kt
+++ app/src/main/java/net/ischool/isus/io/BLEDeviceInfo.kt
@@ -1,5 +1,7 @@
 package net.ischool.isus.io
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * BLE设备信息
  *
@@ -11,21 +13,29 @@ package net.ischool.isus.io
  */
 data class BLEDeviceInfo(
     // 设备名称
+    @SerializedName("dn")
     val deviceName: String,
     // 学校 ID
+    @SerializedName("si")
     val schoolId: String,
     // 学校名称
+    @SerializedName("sn")
     val schoolName: String,
     // 班级 ID
+    @SerializedName("ci")
     val classId: String,
     // 班级名称
+    @SerializedName("cn")
     val className: String,
     // 设备 ID
+    @SerializedName("cmdb")
     val cmdbId: String,
     // 设备 IP
     val ip: String,
     // 设备的自诊断状态
+    @SerializedName("diag")
     val diagState: Int,
     // 设备是否处于休眠中
+    @SerializedName("sleep")
     val isSleep: Boolean,
 )
